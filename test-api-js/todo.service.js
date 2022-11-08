c256e970f9944d2fd68f1e16edb73e5270fa1ed3
kishorekumar44556 <113061251+kishorekumar44556@users.noreply.github.com>
2022-11-07 21:59:26 -0600

    Made changes in the code

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -24,20 +24,20 @@ class todoservice{
 
     add_todo(todo){
         // Your code here
-        this.todo_data.todo[3] = todo;
-        return this.todo_data.todo;
+        this.todo_data.todo[1] = todo;
+        this.todo_data.todo;
     }
 
     delete_todo(id){
         // Your code here
         this.todo_data.todo[id] = {};
-        return this.todo_data.todo;
+        this.todo_data.todo;
     }
 
     update_todo(id, todo){
         // Your code here
         this.todo_data.todo[id] = todo;
-        return this.todo_data.todo;
+        this.todo_data.todo;
     }
 }
 
